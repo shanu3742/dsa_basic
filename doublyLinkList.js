de639c85fc978c74befc119c7cd5452c35c9e1b8
kumar shanu <kumar.shanu@stockedge.com>
2024-08-29 14:17:33 +0530

    insert method added

--- doublyLinkList.js
+++ doublyLinkList.js
@@ -56,6 +56,50 @@ class DoublyLinkList {
         }
         this.size= this.size+1;
     }
+
+    insert(data,index){
+        if(index<0 || index>this.size){
+            throw new Error('Invalid Index');
+        }
+
+        let node = new Node(data)
+
+      if(index===0){
+        //append at head
+        if(this.getSize()===0){
+            this.prepend(data);
+            //no need to run further to increase the size we allready increase the size in prepend method 
+            return
+        }else{
+            node.prev= this.head;
+            node.next= this.head.next;
+            this.head.next = node;  
+        }
+
+        
+      }else if(index === this.getSize()){
+        //append at end
+        this.append(data);
+        return
+      }else{
+        let insertIndex=0;
+        let currentHead = this.head;
+       
+        while(insertIndex<index-1){
+          currentHead = currentHead.next;
+          insertIndex=insertIndex+1
+        }
+        // currentHead.next=node;
+        node.prev=currentHead;
+        node.next = currentHead.next;
+        currentHead.next=node;
+        currentHead.next.next=node.next;
+        // this.head=node;
+
+      }
+
+      this.size= this.size+1;
+    }
     
     removeFromFront(){
         if(this.isEmpty()){
@@ -128,6 +172,17 @@ console.log(doublyLinkList.print())
 console.log(doublyLinkList.removeFromEnd())
 console.log(doublyLinkList.print())
 
+doublyLinkList.insert(101,1)
+
+console.log(doublyLinkList.print())
+console.log(doublyLinkList.getSize())
+doublyLinkList.insert(303,0);
+console.log(doublyLinkList.print())
+console.log(doublyLinkList.getSize());
+doublyLinkList.insert(1001,5);
+
+// console.log()
 //add at specific index
 //remove at specific index
 //search
+
